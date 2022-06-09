76863a16b37aeacf0d45916f0da9c7d13147327a
Adeola Ephraim <dunsin.bada@gmail.com>
2022-06-09 08:34:38 +0100

    added stop and play func

--- app.js
+++ app.js
@@ -17,15 +17,41 @@ const toggleBackgrounds = () => {
     body.style.backgroundImage = `url(${imageUrl})`;
 };
 
-// Play audio
-const playAudio = () => {
-    myAudio = new Audio("./assets/audio/1.mp3");
-    myAudio.loop = true;
-    myAudio.play();
+const backgroundMusic = new Audio("./assets/audio/1.mp3"); // Audio
+const toggleMusicButton = document.getElementById("toggle-sound-btn"); // Toggle button
+const toggleMusicButtonText = document.getElementById("toggle-sound-btn-text"); // Toggle button text
+const musicPlayingSpan = document.getElementById("music-playing-dot"); // Music playing dot
+
+// Start audio
+const startAudio = () => {
+    backgroundMusic.loop = true;
+    backgroundMusic.play();
+    toggleMusicButtonText.innerText = "Stop Sound";
+    musicPlayingSpan.classList.remove("hidden");
 };
 
+// Stop audio
+const stopAudio = () => {
+    backgroundMusic.pause();
+    backgroundMusic.currentTime = 0;
+    toggleMusicButtonText.innerText = "Play Sound";
+    musicPlayingSpan.classList.add("hidden");
+};
+
+// Play Music
+const playAudio = () => {
+    stopAudio();
+    startAudio();
+};
+
+// Click Play button
+toggleMusicButton.addEventListener("click", () => {
+    if (backgroundMusic.paused || backgroundMusic.currentTime == 0)
+        return playAudio();
+    return stopAudio();
+});
+
 toggleBackgrounds(); // Run
-playAudio(); // Run
 
 // Change Background image every x seconds
 setInterval(toggleBackgrounds, 10000);
